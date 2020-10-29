27185614b4c5c252d51b8ec4f825b40b5b25d04e
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2020-10-29 20:48:54 +0900

    java
    2020.10.29

---
+++ test/src/p14_io/MenuPaneP.java
@@ -0,0 +1,12 @@
+package p14_io;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MenuPaneP extends JMenuBar {
+
+	private JMenu fileM, editM,viewM;
+	private JMenuItem newM, openM, saveM, exitM, cutM, copyM, pasteM;
+	
+}
